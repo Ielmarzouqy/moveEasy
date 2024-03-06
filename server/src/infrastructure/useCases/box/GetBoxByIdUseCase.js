95382efaa4a0bd8d1beb1e3ce347a2b8a96eb023
Ielmarzouqy <elmarzouqyimane@gmail.com>
2024-03-06 09:39:08 +0100

    get box by id usecase

---
+++ server/src/infrastructure/useCases/box/GetBoxByIdUseCase.js
@@ -0,0 +1,27 @@
+const BoxServices = require("../../../adapters/services/box/BoxServices");
+
+class GetBoxByIdUseCase {
+  constructor() {
+    this.boxServices = new BoxServices();
+  }
+
+  execute = async (boxId) => {
+    try {
+      const menu = await this.boxServices.getBoxById(boxId);
+
+      return {
+        status: 200,
+        message: "box retrieved successfully",
+        box: box,
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        status: error.status || 500,
+        message: error.message || "Something went wrong, please try again",
+      };
+    }
+  };
+}
+
+module.exports = GetBoxByIdUseCase;
