65e5439274dee760071355c5b1da02a85328badc
Ielmarzouqy <elmarzouqyimane@gmail.com>
2024-03-27 14:56:59 +0000

    add test for updatecar

--- server/tests/car.test.js
+++ server/tests/car.test.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const CarController = require('../src/adapters/controllers/car/CarController');
 const CreateCarUseCase = require('../src/infrastructure/useCases/car/CreateCarUseCase');
 const GetAllCarsUseCase = require('../src/infrastructure/useCases/car/GetAllCarsUseCase');
+const UpdateCarUseCase = require('../src/infrastructure/useCases/car/UpdateCarUseCase');
 
 // Mock the use case modules
 jest.mock('../src/infrastructure/useCases/car/CreateCarUseCase');
 jest.mock('../src/infrastructure/useCases/car/GetAllCarsUseCase');
+jest.mock('../src/infrastructure/useCases/car/UpdateCarUseCase');
 
 const originalConsole = global.console;
 
@@ -22,9 +24,9 @@ describe('CarController', () => {
     };
   });
 
-  afterAll(() => {
+  afterAll(async () => {
     global.console = originalConsole;
-    return mongoose.connection.close();
+    return await mongoose.connection.close();
   });
 
   beforeEach(() => {
@@ -98,4 +100,53 @@ describe('CarController', () => {
       expect(GetAllCarsUseCase.prototype.execute).toHaveBeenCalledTimes(1);
     });
   });
+
+  describe('updateCar', () => {
+    beforeEach(() => {
+      // Reset mock and set up mock implementation for updateCarUseCase
+      UpdateCarUseCase.mockClear();
+      UpdateCarUseCase.prototype.execute = jest.fn().mockResolvedValue({
+        status: 200,
+        message: 'Car updated successfully',
+        car: {
+          id: 'carId123',
+          name: 'updatedCar',
+          price: 200,
+          category: 'updatedCategory',
+        },
+      });
+
+      // Assuming updateCarUseCase is an instance of UpdateCarUseCase in your CarController
+      carController.updateCarUseCase = new UpdateCarUseCase();
+    });
+
+    it('should update a car and return the updated car with a status code of 200', async () => {
+      const req = {
+        params: { carId: 'carId123' },
+        body: { name: 'updatedCar', price: 200, category: 'updatedCategory' },
+      };
+      const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+
+      await carController.updateCar(req, res);
+
+      // Check if execute method of UpdateCarUseCase was called with correct parameters
+      expect(UpdateCarUseCase.prototype.execute).toHaveBeenCalledWith(req.params.carId, req.body);
+      // Check if the response status and json methods were called with expected values
+      expect(res.status).toHaveBeenCalledWith(200);
+     
+      expect(res.json).toHaveBeenCalledWith({
+        message: 'Car updated successfully',
+        car: {
+          id: 'carId123', // Include the id in the expectation
+          name: 'updatedCar',
+          price: 200,
+          category: 'updatedCategory',
+        },
+      });
+
+      // Optionally, verify that the execute method of UpdateCarUseCase was called once
+      expect(UpdateCarUseCase.prototype.execute).toHaveBeenCalledTimes(1);
+    });
+  });
 });
+
